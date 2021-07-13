9784bf7010f9149de693572dd20a681f3e94ad1b
Вова Пича <86943253+VovaPicha@users.noreply.github.com>
2021-07-14 00:35:36 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,22 +8,24 @@ def main():
         except ValueError:
             words.append(word)
     print(' '.join(words))
+    # виводим числа
     print(nums)
-    for i in words:
-        i[0].upper() + i[1:-1] + i[-1].upper()
-    words = list(map(lambda x: x.apper() if len(x) == 1 else x[0].upper() + x[1:-1] + x[-1].upper(), words))
+    words = list(map(lambda x: x.upper() if len(x) == 1 else x[0].upper() + x[1:-1] + x[-1].upper(), words))
     print(' '.join(words))
-    idx = 0
-    for i in range(len(nums)):
-        if nums[idx] < nums[i]:
-            idx = i
-    print('Idx: {}, Num: {}'.format(idx, nums[idx]))
-    numsPow=[]
-    for i in range(len(nums)):
-        if i == idx:
-            continue
-        numsPow.append(pow(nums[i], i))
-    print(numsPow)
+    if len(nums) == 0:
+        print('No nums in string')
+    else:
+        idx = 0
+        for i in range(len(nums)):
+            if nums[idx] < nums[i]:
+                idx = i
+        print('Idx: {}, Num: {}'.format(idx, nums[idx]))
+        numsPow=[]
+        for i in range(len(nums)):
+            if i == idx:
+                continue
+            numsPow.append(pow(nums[i], i))
+        print(numsPow)
 
 
 if __name__ == '__main__':
