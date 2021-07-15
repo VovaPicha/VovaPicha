4ef55a319eace79cdfb667d216ec2b98f8d5be52
Вова Пича <86943253+VovaPicha@users.noreply.github.com>
2021-07-16 00:14:25 +0300

    Add files via upload

---
+++ task3/task3.py
@@ -0,0 +1,68 @@
+import http.client
+import json
+from tkinter import *
+import tkinter as tk
+
+
+root = Tk()
+root.title("Ситуація COVID-19")
+root.geometry('300x800')
+root['bg']='#C0C0C0'
+
+n=8 
+
+conn = http.client.HTTPSConnection("vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "389cce55e5mshb1a04a2368c255ep1b2c85jsnf5f7f9efb26b",
+    'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+    }
+
+conn.request("GET", "/api/npm-covid-data/europe", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+All_Info = data.decode("utf-8")
+json = json.loads(All_Info)
+
+frametop=Frame(root)
+framebot=Frame(root)
+frametop.pack()
+framebot.pack()
+
+def Ref():
+    import json
+    TextBox.delete(1.0,END)
+    conn.request("GET", "/api/npm-covid-data/europe", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    All_Info = data.decode("utf-8")
+    json = json.loads(All_Info)
+    TextBox.insert('1.0', '\n')
+    
+    for i in range(n):
+        TextBox.insert('1.0', '\n')
+        TextBox.insert('1.0', list(json[i].items())[14])
+        TextBox.insert('1.0', '\n')
+        TextBox.insert('1.0', list(json[i].items())[12])
+        TextBox.insert('1.0', '\n')
+        TextBox.insert('1.0', list(json[i].items())[10])
+        TextBox.insert('1.0', '\n')
+        TextBox.insert('1.0', list(json[i].items())[3])
+        TextBox.insert('1.0', '\n')
+        TextBox.insert('1.0', list(json[i].items())[2])
+        TextBox.insert('1.0', '\n')
+        TextBox.insert('1.0', '\n')
+
+Refbutton = Button(frametop, text="Оновлення",command=Ref)
+Refbutton['bg']='#C0C0C0'
+Refbutton['fg']='#800000'
+Refbutton.pack(side=LEFT)
+
+TextBox = Text(framebot,width=500, height=500)
+TextBox['bg']='#FFFFFF'
+TextBox['fg']='#000000'
+TextBox.pack()
+
+Ref()
+root.mainloop()
